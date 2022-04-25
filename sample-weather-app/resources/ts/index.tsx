12c67c609544a601b5dfe108dc137937427a4ed2
haku <boozerby@gmail.com>
2022-04-26 00:11:47 +1000

    fixed ts options

--- sample-weather-app/resources/ts/index.tsx
+++ sample-weather-app/resources/ts/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { DefaultApi } from '../typescript-axios/api/default-api';
 
 const App = () => {
+    const api = new DefaultApi();
+    (async () => {
+        const data = await api.fetchData();
+        console.log(data);
+    })()
+    
     return (
         <h1>Laravel SPA</h1>
     )
 }
+
 
 ReactDOM.render(
     <App />,
